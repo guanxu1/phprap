ac1062383a842bcf7ed963ec28671f24bb855be9
够过瘾 <245629560@qq.com>
2017-10-30 16:55:50 +0800

    去掉安装步骤一mysql版本检测

--- application/install/controller/step1.php
+++ application/install/controller/step1.php
@@ -4,7 +4,6 @@ namespace app\install\controller;
 
 use gophp\request;
 use gophp\response;
-use gophp\schema;
 
 class step1 extends auth {
 
@@ -41,7 +40,6 @@ class step1 extends auth {
             $system = [
                 'php_os' => PHP_OS,
                 'php_version'   => PHP_VERSION,
-                'mysql_version' => schema::instance()->version(),
             ];
 
             $chmod['runtime'] = get_dir_chmod(ROOT_PATH.'/runtime/');
